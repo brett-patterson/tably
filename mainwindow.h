c3aa2d3c652bfd7c94ac3933fea302bc04047960
Brett Patterson <brett.patterson@rice.edu>
2018-08-03 15:28:08 -0400

    Add header documentation

--- mainwindow.h
+++ mainwindow.h
@@ -17,11 +17,18 @@ public:
     ~MainWindow();
 
 public slots:
+    /**
+     * Opens the file dialog to load a tab.
+     */
     void openTabDialog();
 
 private:
     Ui::MainWindow *ui;
 
+    /**
+     * Load a tab from a file.
+     * @param filename The path to the file to load
+     */
     void loadTab(std::string filename);
 };
 
--- tabeditor.h
+++ tabeditor.h
@@ -11,7 +11,13 @@ class TabEditor : public QWidget
     Q_OBJECT
 public:
     explicit TabEditor(QWidget *parent = nullptr);
+
+    /**
+     * Set the tab displayed in the editor.
+     * @param tab The tab to display
+     */
     void setTab(Tab *tab);
+
     ~TabEditor();
 
 protected:
@@ -22,13 +28,46 @@ signals:
 public slots:
 
 private:
+    /**
+     * The current tab in the editor.
+     */
     Tab *tab;
 
+    /**
+     * Draw the staff and measures for the tab.
+     * @param p The painter used to draw
+     */
     void drawMeasures(QPainter &p);
+
+    /**
+     * Draw a staff.
+     * @param p The painter used to draw
+     * @param index The index of the staff within the tab
+     */
     void drawStaff(QPainter &p, int index);
+
+    /**
+     * Draw a chord at a given position.
+     * @param p The painter used to draw
+     * @param chord The chord to draw
+     * @param x The x position of the chord
+     * @param line The line in the tab for the chord
+     */
     void drawChord(QPainter &p, const Chord& chord, int x, int line);
+
+    /**
+     * Draw a note at a given position.
+     * @param p The painter used to draw
+     * @param note The note to draw
+     * @param x The x position of the note
+     * @param line The line in the tab for the note
+     */
     void drawNote(QPainter &p, const Note& note, int x, int line);
 
+    /**
+     * Get the height of a staff for the current tab.
+     * @return The staff height
+     */
     int staffHeight();
 };
 
